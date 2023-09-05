6f126fafc3aaa4a1f4d5777a0bf4095df6c5c713
JT Reyes <jesustgreyes@gmail.com>
2023-09-05 11:01:53 -0700

    use dependencies

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
